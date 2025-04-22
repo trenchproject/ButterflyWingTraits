23d11bf8469c8629980277b24008a8cbc696779f
Lauren Buckley <l.b.buckley@gmail.com>
2025-04-22 14:22:27 -0700

    update analysis

--- ProcessOutput.R
+++ ProcessOutput.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(reshape2)
 library(sjPlot)
+library(viridis)
 
 desktop<- "y"
 
@@ -22,7 +23,6 @@ gray[gray==0]<- NA
 gray$side<- NA
 gray$side[grep("0_dorsal", gray$image)] <-"dorsal"
 gray$side[grep("1_ventral", gray$image)] <-"ventral"
-#gray$side<- as.factor(gray$side)
 
 #account for split images
 gray$butterfly.n<- NA
@@ -34,26 +34,11 @@ gray$butterfly.n[grep("2_butterfly", gray$image)] <-2
 gray$downloadID.m<- sub("--.*", "", gray$image)
 bdat$downloadID.m<- gsub(".jpg", "", bdat$downloadID)
   
-#match
-match1<- match(bdat$downloadID.m, gray$downloadID.m)
-bdat$gray_rfw <-NA
-bdat$gray_lfw <-NA
-bdat$gray_rhw <-NA
-bdat$gray_lhw <-NA
-bdat$length_rfw <-NA
-bdat$length_lfw <-NA
-bdat$length_rhw <-NA
-bdat$length_lhw<-NA
-#add data
-bdat$side[which(!is.na(match1))] <- gray$side[na.omit(match1)]
-bdat$gray_rfw[which(!is.na(match1))] <- gray$gray_rfw[na.omit(match1)]
-bdat$gray_lfw[which(!is.na(match1))] <- gray$gray_lfw[na.omit(match1)]
-bdat$gray_rhw[which(!is.na(match1))] <- gray$gray_rhw[na.omit(match1)]
-bdat$gray_lhw[which(!is.na(match1))] <- gray$gray_lhw[na.omit(match1)]
-bdat$length_rfw[which(!is.na(match1))] <- gray$length_rfw[na.omit(match1)]
-bdat$length_lfw[which(!is.na(match1))] <- gray$length_lfw[na.omit(match1)]
-bdat$length_rhw[which(!is.na(match1))] <- gray$length_rhw[na.omit(match1)]
-bdat$length_lhw[which(!is.na(match1))] <- gray$length_lhw[na.omit(match1)]
+#merge data
+bdatm<- merge(x=bdat, y=gray, by = "downloadID.m", all=TRUE)
+
+#lots of images not downloaded?
+bdat<- bdatm[which(!is.na(bdatm$image)),]      
 
 #-------------
 #PLOT
@@ -66,36 +51,69 @@ bdat$length_fw<- rowMeans(bdat[,c("length_rfw","length_lfw")])
 bdat$length_hw<- rowMeans(bdat[,c("length_rhw","length_lhw")])
 
 #to long format
-bdat.l <- melt(bdat[,c("downloadID.m","date","lat","lon","doy","year","species","side","length_fw","length_hw")], 
-               id.vars = c("downloadID.m","date","lat","lon","doy","year","species","side"), variable.name = "wing", value.name="length")
-bdat.g <- melt(bdat[,c("downloadID.m","date","lat","lon","doy","year","species","side","gray_fw","gray_hw")], 
-               id.vars = c("downloadID.m","date","lat","lon","doy","year","species","side"), variable.name = "wing", value.name="gray")
+bdat.l <- melt(bdat[,c("downloadID.m","date","lat","lon","doy","year","species","side",'length_rfw', 'length_lfw', 'length_rhw', 'length_lhw')], 
+               id.vars = c("downloadID.m","date","lat","lon","doy","year","species","side"), variable.name = "label", value.name="length")
+bdat.g <- melt(bdat[,c("downloadID.m","date","lat","lon","doy","year","species","side",'gray_rfw', 'gray_lfw', 'gray_rhw', 'gray_lhw')], 
+               id.vars = c("downloadID.m","date","lat","lon","doy","year","species","side"), variable.name = "label", value.name="gray")
+#code wings
+bdat.l$wing<- "fw"
+bdat.l$wing[grep("hw", bdat.l$label)]<- "hw"
+bdat.g$wing<- "fw"
+bdat.g$wing[grep("hw", bdat.l$label)]<- "hw"
+
+bdat.l$lr<- "left"
+bdat.l$lr[grep("_r", bdat.l$label)]<- "right"
+bdat.g$lr<- "left"
+bdat.g$lr[grep("_r", bdat.g$label)]<- "right"
+
+#convert grayscale 0-255 to percent
+bdat.g$grayscale= (255-bdat.g$gray)/255
+
+#drop NA side
+bdat.g <- bdat.g[-is.na(bdat.g$side),]
+bdat.l <- bdat.l[-is.na(bdat.l$side),]
+
+bdat.g$wing<- as.factor(bdat.g$wing)
+bdat.l$wing<- as.factor(bdat.l$wing)
+bdat.g$lr<- as.factor(bdat.g$lr)
+bdat.l$lr<- as.factor(bdat.l$lr)
+bdat.g$side<- factor(bdat.g$side, levels=c("dorsal","ventral"))
+bdat.l$side<- factor(bdat.l$side, levels=c("dorsal","ventral"))
+
+#fix negative latitudes?
+bdat.g$lat<- abs(bdat.g$lat)
+bdat.l$lat<- abs(bdat.l$lat)
 
 #plot
-ggplot(bdat.g, aes(x=gray, color=wing)) + 
-  facet_wrap(.~species)+
-  geom_density()
+#gray #bdat.g[which(bdat.g$side=="dorsal"),]
+colors= viridis(5, option="mako")[c(2:4)]
 
-#gray
-ggplot(bdat.g[which(bdat.g$side=="dorsal"),], aes(x=doy, y=gray, color=wing)) + 
-  facet_grid(.~species)+
-  geom_point()+geom_smooth(method="lm")
+gdat<- bdat.g[which(bdat.g$species== c("P. occidentalis","P. rapae")[2] & bdat.g$side=="dorsal"),]
+gdat<- gdat[, c("grayscale","doy","lat","wing","side","year","lr")]
+gdat<- na.omit(gdat)
 
-mod= lm(gray ~ doy*lat*wing, data= bdat.g[which(bdat.g$species=="P. occidentalis"),]) 
-mod= lm(gray ~ doy*lat*wing, data= bdat.g[which(bdat.g$species=="P. rapae"),]) 
+ggplot(bdat.g[which(bdat.g$species== c("P. occidentalis","P. rapae")[2]),], aes(x=doy, y=grayscale, color=lr)) + 
+  facet_grid(wing~side)+
+  geom_point()+geom_smooth(method="lm")+theme_bw()+scale_colour_manual(values=colors)
+
+mod= lm(grayscale ~ doy*lat*wing*year+lr, data= gdat) 
 anova(mod)
 
-plot_model(mod, type = "pred", terms = c("doy", "lat", "wing"), show.data=TRUE)
+plot_model(mod, type = "pred", terms = c("doy","year"), show.data=TRUE)
+plot_model(mod, type = "pred", terms = c("doy","lat"), show.data=TRUE)
+plot_model(mod, type = "pred", terms = c("lat","year"), show.data=TRUE)
 
 #length
-ggplot(bdat.l[which(bdat.g$side=="dorsal"),], aes(x=doy, y=length, color=wing)) + 
-  facet_grid(.~species)+
-  geom_point()+geom_smooth(method="lm")
+ggplot(bdat.l[which(bdat.l$species== c("P. occidentalis","P. rapae")[2]),], aes(x=doy, y=length, color=lr)) + 
+  facet_grid(wing~side)+
+  geom_point()+geom_smooth(method="lm")+theme_bw()+scale_colour_manual(values=colors)
 
-mod= lm(length ~ doy*lat*wing, data= bdat.l[which(bdat.g$species=="P. occidentalis"),]) 
-mod= lm(length ~ doy*lat*wing, data= bdat.l[which(bdat.g$species=="P. rapae"),]) 
+mod= lm(length ~ doy*lat*wing*year+lr, data= bdat.l[which(bdat.l$species== c("P. occidentalis","P. rapae")[2] & bdat.g$side=="dorsal"),]) 
 anova(mod)
 
 plot_model(mod, type = "pred", terms = c("doy", "lat", "wing"), show.data=TRUE)
 
-
+plot_model(mod, type = "pred", terms = c("doy","year"), show.data=TRUE)
+plot_model(mod, type = "pred", terms = c("doy","lat"), show.data=TRUE)
+plot_model(mod, type = "pred", terms = c("lat","year"), show.data=TRUE)
+plot_model(mod, type = "pred", terms = c("wing","year"), show.data=TRUE)
